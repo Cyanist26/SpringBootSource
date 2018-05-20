69951dee812023ffb0bf5633dfd67af82ddcdd4f
Cyanist <cuiwq26@outlook.com>
2018-05-20 17:28:32 +0800

    init project

---
+++ highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch1/aop/DemoAnnotationService.java
@@ -0,0 +1,11 @@
+package com.wisely.highlight_spring4.ch1.aop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoAnnotationService {
+    
+    @Action(name = "ע��ʽ���ص�add����")
+    public void add() {}
+    
+}

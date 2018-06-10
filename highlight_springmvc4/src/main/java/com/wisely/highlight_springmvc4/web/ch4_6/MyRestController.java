ba68af7ba8218ad153cd4dea656edec479565b66
Cyanist <cuiwq26@outlook.com>
2018-06-10 12:54:17 +0800

    fix bug

--- highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_6/MyRestController.java
+++ highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_6/MyRestController.java
@@ -4,8 +4,9 @@ import com.wisely.highlight_springmvc4.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class MyRestController {
     
     @Autowired
--- highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_6/NormalController.java
+++ highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_6/NormalController.java
@@ -2,15 +2,15 @@ package com.wisely.highlight_springmvc4.web.ch4_6;
 
 import com.wisely.highlight_springmvc4.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
+@Controller
 public class NormalController {
     
     @Autowired
     DemoService demoService;
-    
     
     @RequestMapping("/normal")
     public String testPage(Model model) {
